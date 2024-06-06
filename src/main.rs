103a7068b8856dc974e0cb053c63fab3d78f0fcb
Graham Keenan <graham.keenan@outlook.com>
2024-06-06 17:39:27 +0100

    concurrent multiple pings

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,28 @@
 use anyhow::Result;
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
-    net::TcpListener,
+    net::{TcpListener, TcpStream},
 };
+
+async fn handler(mut client: TcpStream) -> Result<()> {
+    loop {
+        let mut buf = vec![0; 4096];
+        let n = client.read(&mut buf).await?;
+        buf.truncate(n);
+        client.write(b"+PONG\r\n").await?;
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let listener = TcpListener::bind("127.0.0.1:6379").await?;
 
     loop {
-        let mut client = match listener.accept().await {
+        let client = match listener.accept().await {
             Ok((client, _)) => client,
             Err(err) => anyhow::bail!("something went wrong: {err}"),
         };
 
-        let mut buf = vec![0; 4096];
-        let n = client.read(&mut buf).await?;
-        buf.truncate(n);
-        client.write(b"+PONG\r\n").await?;
+        tokio::task::spawn(async move { handler(client).await });
     }
 }
