55496e87573d534b0caa49512ac75e587de2fe37
Graham Keenan <graham.keenan@outlook.com>
2024-06-06 17:37:20 +0100

    respond to ping

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
 use anyhow::Result;
-use tokio::net::TcpListener;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpListener,
+};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let listener = TcpListener::bind("127.0.0.1:6379").await?;
 
     loop {
-        let _client = match listener.accept().await {
+        let mut client = match listener.accept().await {
             Ok((client, _)) => client,
             Err(err) => anyhow::bail!("something went wrong: {err}"),
         };
+
+        let mut buf = vec![0; 4096];
+        let n = client.read(&mut buf).await?;
+        buf.truncate(n);
+        client.write(b"+PONG\r\n").await?;
     }
 }
