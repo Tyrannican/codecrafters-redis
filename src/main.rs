d67569935097fd1e491f4f064478ba43a92e3350
Graham Keenan <graham.keenan@outlook.com>
2024-06-07 17:04:30 +0100

    configured listening port

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 use anyhow::Result;
+use clap::Parser;
 
 mod connection;
 mod redis;
-
 use connection::server::RedisNode;
+
+#[derive(Debug, Parser)]
+struct Cli {
+    #[arg(short, long, default_value_t = 6379)]
+    port: u16,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let node = RedisNode::new("127.0.0.1:6379").await?;
+    let cli = Cli::parse();
+    let address = format!("127.0.0.1:{}", cli.port);
+    let node = RedisNode::new(&address).await?;
     node.serve().await
 }
