e981358df32308f2965dd7d892ac38f28e09d627
Graham Keenan <graham.keenan@outlook.com>
2025-07-16 12:04:15 +0100

    respond to ping

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 #![allow(unused_imports)]
+use std::io::Write;
 use std::net::TcpListener;
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                stream.write_all(b"+PONG\r\n")?;
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+
+    Ok(())
 }
