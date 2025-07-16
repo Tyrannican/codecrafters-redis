803396df299331be1a1ca7be4efecc34a0ba74fb
Graham Keenan <graham.keenan@outlook.com>
2025-07-16 15:17:49 +0100

    respond to multiple pings

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,42 @@
-#![allow(unused_imports)]
-use std::io::Write;
-use std::net::TcpListener;
+use anyhow::Result;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
 
-fn main() -> anyhow::Result<()> {
+struct ConnectionHandler {
+    stream: TcpStream,
+}
+
+impl ConnectionHandler {
+    pub fn new(stream: TcpStream) -> Self {
+        Self { stream }
+    }
+
+    pub fn handle_connection(&mut self) -> Result<()> {
+        loop {
+            let mut buf = [0; 4096];
+            let n = self.stream.read(&mut buf)?;
+            if n == 0 {
+                break;
+            }
+
+            self.stream.write_all(b"+PONG\r\n")?;
+        }
+
+        Ok(())
+    }
+}
+
+fn main() -> Result<()> {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                stream.write_all(b"+PONG\r\n")?;
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        let Ok(stream) = stream else {
+            eprintln!("error establishing connection to client");
+            continue;
+        };
+
+        let mut handler = ConnectionHandler::new(stream);
+        handler.handle_connection()?;
     }
 
     Ok(())
